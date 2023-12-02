c504c938fe1ed8edf8d7ea4ba6f87ce82feb7c0a
prefer_choi <prefer_choi@naver.com>
2023-12-03 04:51:34 +0900

    feat: drink detail 페이지 추가

---
+++ web/src/pages/DrinkName.jsx
@@ -0,0 +1,10 @@
+import DrinkDetail from '../components/drink/DrinkDetail.jsx';
+import DefaultLayout from '../components/layout/DefaultLayout.jsx';
+
+export default function DrinkName() {
+  return (
+    <DefaultLayout>
+      <DrinkDetail />
+    </DefaultLayout>
+  );
+}
