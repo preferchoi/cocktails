a0f4305e28a8e0ac669f3a1908df336b17ec7e70
prefer_choi <prefer_choi@naver.com>
2023-12-02 22:00:02 +0900

    feat: 프로바이더를 사용해 최상단에서 apollo 서버와 연결

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
-// import { ApolloProvider } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main'
+import { createApolloClient } from "./apollo/createApolloClient";
+import Main from './pages/Main.jsx'
 
-
+const apolloClient = createApolloClient();
 
 function App() {
   return (
-    //  <ApolloProvider client={apolloClient}> 
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' Component={Main} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
-    //  </ApolloProvider>
+    <ApolloProvider client={apolloClient}>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' Component={Main} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
