1033d0f9c8bc0aaeca5cc2f091209ebce3c1cf41
prefer_choi <prefer_choi@naver.com>
2023-12-02 22:00:23 +0900

    feat: drink 데이터 호출 테스트

--- web/src/pages/Main.jsx
+++ web/src/pages/Main.jsx
@@ -1,5 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react';
+import { useDrinksQuery } from '../generated/graphql.tsx';
 
 export default function Main() {
-    return <Box>box</Box>
+  const { data, loading, error } = useDrinksQuery();
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <Box>
+      {loading &&
+        new Array(6).fill(0).map(x => <div key={x} height={'400px'} />)}
+      {!loading &&
+        data &&
+        data.Drinks?.map((el, idx) => <div key={idx}>{el.name}</div>)}
+      123
+    </Box>
+  );
 }
