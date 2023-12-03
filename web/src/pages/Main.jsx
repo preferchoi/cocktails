6ce4942f34485ea76de85d55353f3be5c0c2eb4c
prefer_choi <prefer_choi@naver.com>
2023-12-04 04:56:33 +0900

    feat: 임시 메인 페이지 적용

--- web/src/pages/Main.jsx
+++ web/src/pages/Main.jsx
@@ -1,5 +1,18 @@
+import { Button, LinkOverlay } from '@chakra-ui/react';
 import DefaultLayout from '../components/layout/DefaultLayout.jsx';
+import { Link } from 'react-router-dom';
 
 export default function Main() {
-  return <DefaultLayout>123</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <div>메인 페이지입니다.</div>
+      <div>해당 웹에 대한 설명을 제공할 예정입니다.</div>
+      <Button>
+        <LinkOverlay as={Link} to={'/signin'}>로그인 하기</LinkOverlay>
+      </Button>
+      <Button>
+        <LinkOverlay as={Link} to={'/signup'}>회원가입 하기</LinkOverlay>
+      </Button>
+    </DefaultLayout>
+  );
 }
