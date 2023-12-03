a39b59354fc58b95cb1ca63de2bd2060144c882d
prefer_choi <prefer_choi@naver.com>
2023-12-04 02:48:51 +0900

    feat: drink detail 페이지 요소 배치, 데이터 유뮤에 따라 다른 출력

--- web/src/components/drink/DrinkDetail.jsx
+++ web/src/components/drink/DrinkDetail.jsx
@@ -1,3 +1,17 @@
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  Table,
+  TableContainer,
+  Tbody,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import { useDrinkQuery } from '../../generated/graphql.tsx';
 
@@ -11,19 +25,103 @@ export default function DrinkDetail() {
   if (error) <p>{error.data}</p>;
 
   return (
-    <>
+    <Box>
       {loading && <p>loading...</p>}
       {!loading && data && (
-        <div>
-          <p>{data.Drink.name}</p>
-          <p>{data.Drink.alcoholic}</p>
-          <p>{data.Drink.category}</p>
-          <p>{data.Drink.glass}</p>
-          <p>{data.Drink.img_path}</p>
-          <p>{data.Drink.ingredients}</p>
-          <p>{data.Drink.instructions}</p>
-        </div>
+        <>
+          <Flex
+            mt={12}
+            flexDirection={{ base: 'column', md: 'row' }}
+            alignContent={'center'}
+          >
+            <Box maxW={250} flex={1}>
+              <Image src={data.Drink.img_path} borderRadius={20} />
+            </Box>
+            <Flex
+              flex={1}
+              ml={{ base: 0, md: 6 }}
+              flexDirection={'column'}
+              alignContent={'center'}
+              justify={'center'}
+              alignItems={'flex-start'}
+            >
+              <Heading mb={4}>{data.Drink.name}</Heading>
+              <Heading size={'md'} mb={2}>
+                {data.Drink.alcoholic} / {data.Drink.category}
+              </Heading>
+              <Heading size={'md'} mb={2}>
+                {data.Drink.glass}
+              </Heading>
+            </Flex>
+          </Flex>
+          <Divider mt={6} />
+          <Heading fontSize={20} mt={2} mb={2}>
+            주조법
+          </Heading>
+          {data.Drink.instructions.length === 0 && (
+            <Text>데이터가 없어요 :(</Text>
+          )}
+          <Flex
+            flexDirection={'column'}
+            alignContent={'center'}
+            justify={'center'}
+            alignItems={'flex-start'}
+            mt={3}
+          >
+            {data.Drink.instructions &&
+              data.Drink.instructions
+                .split(/(\d\.)/)
+                .filter(Boolean)
+                .map((instruction, index, array) => {
+                  if (index % 2 === 0 && index < array.length - 1) {
+                    return (
+                      <Text key={index} fontSize={'sm'}>
+                        {instruction + ' ' + array[index + 1]}
+                      </Text>
+                    );
+                  }
+                  return null;
+                })}
+            {data.Drink.instructions &&
+              data.Drink.instructions.split(/(\d\.)/).filter(Boolean).length ===
+                1 &&
+              data.Drink.instructions
+                .split(/(?<!\boz)\b[.!?]\s*/)
+                .filter(instruction => instruction.trim().length > 0)
+                .map((instruction, index) => (
+                  <Text key={index} fontSize={'sm'}>
+                    {index + 1 + '. ' + instruction.trim() + '.'}
+                  </Text>
+                ))}
+            <Divider mt={6} />
+            <TableContainer mt={6} width="100%">
+              <Heading fontSize={20}>재료 목록</Heading>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>재료</Th>
+                    <Th>용량</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.Drink.ingredients.map(ingredient => (
+                    <Tr>
+                      <Th>{ingredient[0]}</Th>
+                      <Th>{ingredient[1]}</Th>
+                    </Tr>
+                  ))}
+                  {data.Drink.ingredients.length === 0 && (
+                    <Tr>
+                      <Th>데이터가 없어요 :(</Th>
+                      <Th>데이터가 없어요 :(</Th>
+                    </Tr>
+                  )}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Flex>
+        </>
       )}
-    </>
+    </Box>
   );
 }
