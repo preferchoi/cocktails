d52dd39b969b33f658935fa46ef212ec241dc64b
prefer_choi <prefer_choi@naver.com>
2023-12-04 03:01:44 +0900

    feat: IngredientDetail 컴포넌트 데이터 배치

--- web/src/components/ingredient/IngredientDetail.jsx
+++ web/src/components/ingredient/IngredientDetail.jsx
@@ -1,4 +1,5 @@
-import { useParams } from 'react-router-dom';
+import { Divider, Heading, Text, Link as ChakraLink } from '@chakra-ui/react';
+import { Link, useParams } from 'react-router-dom';
 import { useIngredientQuery } from '../../generated/graphql.tsx';
 
 export default function IngredientDetail() {
@@ -11,7 +12,24 @@ export default function IngredientDetail() {
   return (
     <>
       {loading && <p>loading...</p>}
-      {!loading && data && <>{data.Ingredient.name}</>}
+      {!loading && data && (
+        <>
+          <Heading>{data.Ingredient.Ingredient.name}</Heading>
+          <Divider mt={6} mb={6} />
+          <Text mb={2}>
+            {data.Ingredient.Ingredient.name}을 사용하는 칵테일들
+          </Text>
+          <>
+            {data.Ingredient.Drinks.map(drink => (
+              <Text>
+                <ChakraLink as={Link} to={`/drink/${drink.name}`}>
+                  {drink.name}
+                </ChakraLink>
+              </Text>
+            ))}
+          </>
+        </>
+      )}
     </>
   );
 }
