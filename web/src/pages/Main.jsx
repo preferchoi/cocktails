44e9eecf61eedc8123efc0eb802b25370d7b2e94
prefer_choi <prefer_choi@naver.com>
2023-12-04 04:58:59 +0900

    feat: 추가 설명

--- web/src/pages/Main.jsx
+++ web/src/pages/Main.jsx
@@ -7,6 +7,7 @@ export default function Main() {
     <DefaultLayout>
       <div>메인 페이지입니다.</div>
       <div>해당 웹에 대한 설명을 제공할 예정입니다.</div>
+      <div>현재는 회원 기능이 제공되고 있지 않습니다. navBar 요소를 클릭해 페이지를 확인해 보세요.</div>
       <Button>
         <LinkOverlay as={Link} to={'/signin'}>로그인 하기</LinkOverlay>
       </Button>
