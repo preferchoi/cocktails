9c9ecb751f4b7fa1a8c714dcac66553c1ecb3f49
prefer_choi <prefer_choi@naver.com>
2023-12-14 13:30:40 +0900

    feat: 에러링크 설정

--- web/src/apollo/createApolloClient.js
+++ web/src/apollo/createApolloClient.js
@@ -1,7 +1,35 @@
-import { ApolloClient } from "@apollo/client";
+import { ApolloClient, from, HttpLink, NormalizedCacheObject } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { createApolloCache } from "./createApolloCache.js";
 
 export const createApolloClient = () => new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: createApolloCache(),
+  link: from([errorLink, httpLink]),
 });
+
+const errorLink = onError(
+  // eslint-disable-next-line consistent-return
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        // eslint-disable-next-line no-console
+        console.log(`[GraphQL error]: -> ${operation.operationName}
+      Message: ${message}, Query: ${path}, Location: ${JSON.stringify(
+          locations,
+        )}`),
+      );
+    }
+    if (networkError) {
+      // eslint-disable-next-line no-console
+      console.log(`[networkError]: -> ${operation.operationName}
+    Message: ${networkError.message}`);
+    }
+  },
+);
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include',
+});
+
