d083cd5f9515828288d38e98f31fe49ef4f829dc
preferchoi <prefer_choi@naver.com>
2025-07-09 13:46:25 +0900

    origin

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,7 @@ async function main() {
     app, 
     path: '/api/graphql', 
     cors: {
-      origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+      origin: ['*'],
       credentials: true,
     },
   });
